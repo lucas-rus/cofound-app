a20f0ed87c405bf90a59714b24298bbc4915fd35
Lucas <lucas.rus.dojo@gmail.com>
2026-01-06 20:29:56 +0100

    Use env variable for API base URL

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '', // Proxied in vite.config.js to http://localhost:8080
+    baseURL: import.meta.env.VITE_API_BASE_URL || '',
     headers: {
         'Content-Type': 'application/json',
     },
