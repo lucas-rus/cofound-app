78ce512030d0f06c527e83bc0ecf342446908d15
Lucas <lucas.rus.dojo@gmail.com>
2026-01-09 23:34:42 +0200

    Fix: Add sslmode=require to DB URL and log connection details

--- src/main/java/com/cofound/config/RenderDatabaseConfig.java
+++ src/main/java/com/cofound/config/RenderDatabaseConfig.java
@@ -24,7 +24,9 @@ public class RenderDatabaseConfig {
             URI dbUri = new URI(databaseUrl);
             String username = dbUri.getUserInfo().split(":")[0];
             String password = dbUri.getUserInfo().split(":")[1];
-            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+            System.out.println("Attempting to connect to DB: " + dbUrl);
+            System.out.println("DB User: " + username);
 
             return DataSourceBuilder.create()
                     .url(dbUrl)
